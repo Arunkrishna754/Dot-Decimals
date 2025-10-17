511c0f114cecf2f973dd30ebd54020ea6863b5e1
Arunkrishna754 <93565312+Arunkrishna754@users.noreply.github.com>
2025-10-17 11:36:03 +0530

    Update server listen method to bind to 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const PORT = process.env.PORT || 5000;
 
 // Only listen when not in serverless environment
 if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  app.listen(PORT,'0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
 }
 
 // ✅ Export for Vercel (ES Module syntax)
